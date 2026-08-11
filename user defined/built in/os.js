cfe8a4b0a53a72e2967ddbb781331c2971f18b2e
pujitharam1487 <pujitharamreddi@gmail.com>
2026-08-11 16:43:20 +0530

    Delete user defined directory

--- user defined/built in/os.js
+++
@@ -1,5 +0,0 @@
-const os = require("os");
-console.log("Operating System:", os.platform());
-console.log("CPU Architecture:", os.arch());
-console.log("Home Directory:", os.homedir());
-console.log("Total Memory:", os.totalmem());
--- user defined/built in/path.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const filePath = "/home/user/documents/file.txt";
-console.log("File Name:", path.basename(filePath));
-console.log("Directory:", path.dirname(filePath));
-console.log("Extension:", path.extname(filePath));
