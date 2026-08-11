39e425bfe25b774007d9ae61448e3919f1ffbea7
Pujitharam <pujitharamreddi@gmail.com>
2026-08-11 16:35:22 +0530

    Added week5 files

---
+++ user defined/built in/os.js
@@ -0,0 +1,5 @@
+const os = require("os");
+console.log("Operating System:", os.platform());
+console.log("CPU Architecture:", os.arch());
+console.log("Home Directory:", os.homedir());
+console.log("Total Memory:", os.totalmem());
